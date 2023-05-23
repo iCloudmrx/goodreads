10b7925f84ac32c59c2d068d38fd4e1466c748e3
Nurillo Mahmudjonov <72136850+iCloudmrx@users.noreply.github.com>
2023-05-23 17:10:55 +0500

    add paginator for comment

--- goodreads/views.py
+++ goodreads/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from books.models import Book_Review
+from django.core.paginator import Paginator
 
 
 def home(request):
-    return render(request, 'goodreads/index.html')
+    reviews = Book_Review.objects.all().order_by('-created_at')
+    page_size = request.GET.get('page_size', 2)
+    paginator = Paginator(reviews, page_size)
+    page_num = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_num)
+    return render(request, 'goodreads/index.html', {
+        'reviews': page_obj
+    })
